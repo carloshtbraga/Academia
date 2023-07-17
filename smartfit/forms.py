816152aeb8f82baa7ac01e9f40e4a683874c1422
carloshtbraga <carloshtbraga@gmail.com>
2023-07-16 23:23:14 -0300

    :sparkles:

--- smartfit/forms.py
+++ smartfit/forms.py
@@ -2,7 +2,20 @@ from django import forms
 from .models import Academia
 
 
+class DateInput(forms.DateInput):
+    input_type = "date"
+
+
 class Formulario(forms.ModelForm):
     class Meta:
         model = Academia
-        fields = ["nome", "endereco", "telefone", "foto"]
+        fields = ["nome", "endereco", "telefone", "foto", "plano", "nascimento"]
+        widgets = {
+            "nome": forms.TextInput(attrs={"placeholder": "Digite o nome do aluno"}),
+            "telefone": forms.TextInput(
+                attrs={"placeholder": "Digite o número de telefone"}
+            ),
+            "nascimento": DateInput(),
+            "endereco": forms.TextInput(attrs={"placeholder": "Digite o endereço"}),
+            "foto": forms.TextInput(attrs={"placeholder": "Digite a URL da foto"}),
+        }
--- smartfit/models.py
+++ smartfit/models.py
@@ -3,10 +3,20 @@ from django.db import models
 
 # Create your models here.
 class Academia(models.Model):
-    nome = models.CharField(max_length=200)
-    endereco = models.CharField(max_length=200)
-    telefone = models.CharField(max_length=200)
-    foto = models.CharField(max_length=300, default='')
+    PLANO_CHOICES = (
+        ("Mensal", "Mensal"),
+        ("Semestral", "Semestral"),
+        ("Anual", "Anual"),
+    )
+
+    nome = models.CharField(max_length=30)
+    endereco = models.CharField(max_length=40)
+    telefone = models.CharField(max_length=15)
+    plano = models.CharField(
+        max_length=10, choices=PLANO_CHOICES, default="Mensal"
+    )
+    nascimento = models.DateField(blank=True, null=True)
+    foto = models.CharField(max_length=300, default="")
 
     def __str__(self):
         return self.nome
